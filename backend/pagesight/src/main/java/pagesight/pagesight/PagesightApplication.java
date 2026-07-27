94543d6e76f5d7f62b17e3daad045c876260ae5b
Aradhya Sharma <aradhyasharmaa96@gmail.com>
2026-07-27 09:35:51 +0530

    update

---
+++ backend/pagesight/src/main/java/pagesight/pagesight/PagesightApplication.java
@@ -0,0 +1,13 @@
+package pagesight.pagesight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagesightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagesightApplication.class, args);
+	}
+
+}
---
+++ backend/pagesight/src/test/java/pagesight/pagesight/PagesightApplicationTests.java
@@ -0,0 +1,13 @@
+package pagesight.pagesight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagesightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
